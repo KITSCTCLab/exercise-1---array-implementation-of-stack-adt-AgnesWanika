ce5f5d9e13b822e5361c6789561b9933ecbbae0b
URK21CS2002 <109648340+URK21CS2002@users.noreply.github.com>
2022-08-02 21:59:22 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,18 @@
 import os
 class Stack:
-    def __init__(self, size):
+    def __init__(self, A):
         self.items = [None]*size
         self.size = size
         self.top = -1
 
     def is_empty(self):
-        if (self.top == -1):
+        if (self.top ==A -1):
             return True
         else:
             return False
 
     def is_full(self):
-        if (self.top == self.size-1):
+        if (self.top == self.A-1):
             return True
         else:
             return False
@@ -34,7 +34,7 @@ class Stack:
 
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
+stack = Stack(A)
 for line in range(queries):
     values = list(map(int, input().rstrip().split()))
     if values[0] == 1:
